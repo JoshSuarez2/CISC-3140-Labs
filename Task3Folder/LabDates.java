a73cc90c456cd18c4b381af49ac8fff46e709999
Joshua Suarez <joshsuarez.1218@gmail.com>
2022-09-26 15:32:42 -0400

    Tasks

---
+++ Task3Folder/LabDates.java
@@ -0,0 +1,12 @@
+public class LabDates {
+	public static void main(String [] args) {
+		int Lab1 = 0;
+		int Lab2 = 28;
+		int Lab3 = 56;
+		int Lab4 = 70;
+		
+		System.out.println("The days between Lab 1 and 2 = "+(Lab2-Lab1)+" days");
+		System.out.println("The days between Lab 2 and 3 = "+(Lab3-Lab2)+" days");
+		System.out.println("The days between Lab 3 and 4 = "+(Lab4-Lab3)+" days");
+	}
+}
